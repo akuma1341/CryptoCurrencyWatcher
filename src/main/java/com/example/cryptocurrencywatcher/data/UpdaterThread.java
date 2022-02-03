a681f083ee0ae1abdfc1a6732c57959b9ca8af55
akuma1341 <akuma1341@gmail.com>
2022-02-03 22:20:18 +0300

    In UpdaterThread changed dependencies, and error catch

--- src/main/java/com/example/cryptocurrencywatcher/data/UpdaterThread.java
+++ src/main/java/com/example/cryptocurrencywatcher/data/UpdaterThread.java
@@ -1,22 +1,27 @@
 package com.example.cryptocurrencywatcher.data;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UpdaterThread extends Thread{
+    private static final Logger LOGGER = LoggerFactory.getLogger(PricesUpdater.class);
     @Autowired
-    private PricesUpdater priceUpdater;
+    private CurrencyPricesUpdater currencyPricesUpdater;
+    @Autowired
+    private ControllerOfPriceChangesForUsers controller;
 
     @Override
     public void run() {
         while (true) {
             try {
-                priceUpdater.updatePrices();
-                priceUpdater.checkPriceChangesForUsers();
+                currencyPricesUpdater.updatePrices();
+                controller.checkPriceChangesForUsers();
                 Thread.sleep(60000);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                LOGGER.error("UpdaterThread interrupted: " + e.getMessage());
             }
         }
     }
